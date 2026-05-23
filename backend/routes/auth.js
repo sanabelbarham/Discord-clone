a8609cffd0a0a008e54b88d5513780c6e7a69e7a
Sanabel Barham <sanabelbarham123@gmail.com>
2026-05-23 10:04:42 +0300

    login and register and token in backend sidde

---
+++ backend/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(400).json({ message: "User exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+
+    res.json({ message: "User created" });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Wrong password" });
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token, username: user.username });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const Message = require("./models/Message");
 
 const app = express();
+const authRoutes = require("./routes/auth");
 
+app.use("/api/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
 
