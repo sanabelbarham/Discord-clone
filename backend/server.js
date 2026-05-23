b6c749216d738607abc21207cc8c400b5c6caab6
Sanabel Barham <sanabelbarham123@gmail.com>
2026-05-23 09:52:06 +0300

    messages are stored in the db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const { Server } = require("socket.io");
-require("dotenv").config();
+
+dotenv.config();
+
+const Message = require("./models/Message");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,28 +25,37 @@ const io = new Server(server, {
   }
 });
 
-let messages = [];
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB error:", err));
 
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("User connected:", socket.id);
 
 
+  const messages = await Message.find();
   socket.emit("loadMessages", messages);
 
 
-  socket.on("sendMessage", (data) => {
-    messages.push(data);
+  socket.on("sendMessage", async (data) => {
+    try {
+      const newMessage = new Message(data);
+      await newMessage.save();
 
-  
-    io.emit("receiveMessage", data);
+      io.emit("receiveMessage", newMessage);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
